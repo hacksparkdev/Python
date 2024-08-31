1532c49904d8c2c8728c3bc0ee068456af0a5113
hackspark <hackspark@outlook.com>
2024-08-31 15:34:46 +0000

    Working on files

--- working_with_files/reading_files.py
+++ working_with_files/reading_files.py
@@ -1,1 +1,12 @@
-with open
+# Open file in same directory 
+#with open ('names.txt') as f:
+#   contents =  f.read()
+#   print(contents)
+
+
+# Relative path
+
+with open('textfiles/employee_list.txt') as f:
+    contents = f.read()
+    print(contents)
+
