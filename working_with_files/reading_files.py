30343490324a647be6a857200f485ca291dd19b4
hackspark <hackspark@outlook.com>
2024-09-01 02:44:47 +0000

    more stuff

--- working_with_files/reading_files.py
+++ working_with_files/reading_files.py
@@ -1,4 +1,14 @@
-filename = 'programming.txt'
 
-with open(filename, 'a') as f:
-    f.write('This is appended text')
+
+try:
+    with open("textfiles/antichrist.txt", encoding='utf-8') as f:
+        contents = f.read()
+
+except FileNotFoundError:
+    msg = f'Sorry, the file ${filename} does not exist'
+    print(msg)
+
+else:
+    words = contents.split()
+    num_words = len(words)
+    print(f'The file has {num_words} words.')
