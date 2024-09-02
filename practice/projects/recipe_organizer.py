6d7890a4fc42f82727349099a73fb1326a574b59
hackspark <hackspark@outlook.com>
2024-09-02 15:01:37 +0000

    working on project

--- practice/projects/recipe_organizer.py
+++ practice/projects/recipe_organizer.py
@@ -2,10 +2,30 @@
 import json
 
 # Add, edit, and delete recipe
+recipes = [
+    {
+        "name" : 'Hotdogs',
 
+        "ingrdients":[
+            '1 pack of hotdogs',
+            '1 pot',
+            '1 can of chille'
 
+        ],
+        "Nutrition":{
+            'Calories' : 50,
+            'fat' : 9,
+            'carbs' : 63,
+            'protien': 65
+        },
+        "Description":[
+            "American style hotdogs, Want to take years off of your life Eat these beautifuly awful hotdogs "
+        ]
+    }
+]
 
-
+with open('recipes.json', 'w') as file:
+    json.dump(recipes, file, indent=4)
 
 #nutrition_info = recipes[0]['Nutrition']
 #for key, value in nutrition_info.items():
